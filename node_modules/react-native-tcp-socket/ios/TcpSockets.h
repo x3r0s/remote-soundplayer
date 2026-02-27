220c3e6336561f966aa5c5f7c51337becc4caef5
Xeros <xeros.log@gmail.com>
2026-02-28 01:10:19 +0900

    Add .gitignore, remove node_modules from tracking

--- node_modules/react-native-tcp-socket/ios/TcpSockets.h
+++
@@ -1,9 +0,0 @@
-#import "TcpSocketClient.h"
-
-#import "CocoaAsyncSocket/GCDAsyncSocket.h"
-#import <React/RCTEventEmitter.h>
-
-
-@interface TcpSockets : RCTEventEmitter <SocketClientDelegate>
-
-@end
